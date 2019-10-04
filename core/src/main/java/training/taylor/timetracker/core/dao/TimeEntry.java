8f9f8833d13a1c136223440bcecf585875685f12
Josué Son <43652848+Labnos@users.noreply.github.com>
2019-10-04 08:41:15 -0600

    Update TimeEntry.java

--- core/src/main/java/training/taylor/timetracker/core/dao/TimeEntry.java
+++ core/src/main/java/training/taylor/timetracker/core/dao/TimeEntry.java
@@ -42,7 +42,7 @@ public class TimeEntry {
         return "TimeEntry{" +
                 "description='" + description + '\'' +
                 ", rate=" + rate +
-                ", time=" + time 
+                ", time=" + time +
                 '}';
     }
 }
